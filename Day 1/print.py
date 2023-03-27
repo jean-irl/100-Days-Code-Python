f468289573a6b03c19b0deb3b1a5abbccb15692a
jean-irl <andres.ira.041093@gmail.com>
2023-03-27 14:06:57 -0700

    Day 1 Progress

--- Day 1/print.py
+++ Day 1/print.py
@@ -1,9 +1,11 @@
 # print function in python
-print('hello world')
+hello = 'hello world'
 
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
+
+print(len(hello))
 
 
 
