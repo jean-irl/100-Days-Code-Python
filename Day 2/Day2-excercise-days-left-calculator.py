2aab68a114c9a4b944f35363735224c6bb1a753d
jean-irl <andres.ira.041093@gmail.com>
2023-03-28 12:50:44 -0700

    Days until you die excercise finished

---
+++ Day 2/Day2-excercise-days-left-calculator.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# Works until age 90
+
+max_age = 90
+
+max_days = max_age * 365
+max_weeks = max_age * 52
+max_months = max_age * 12
+
+days = int(age) * 365
+weeks = int(age) * 52
+months = int(age) * 12
+
+
+
+days_left = max_days - days
+weeks_left = int(max_weeks - weeks)
+months_left = int(max_months - months)
+
+
+print("You have " + str(days_left) + " days, " + str(weeks_left) + " weeks, and " + str(months_left) + " months left.")
---
+++ Day 2/python-operators.py
@@ -0,0 +1,22 @@
+#adding
+3+4
+
+#substract
+4-3
+
+#Multiplication
+3*2
+
+#division
+print(6/3);
+
+#power
+print(2**2)
+
+#PEMDAS
+# ()
+# **
+# /*
+# +-
+
+print(3*3+3/3-3)
